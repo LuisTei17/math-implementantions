06699ef8197ab3def127bb160f45d59fec6190ef
LuisTei17 <luisedteixeira@gmail.com>
2018-11-06 00:50:51 -0200

    Added scalar multiplication

---
+++ matrices/scalarMultiplication.py
@@ -0,0 +1,18 @@
+def scalarMultiplication(matrix, multiplier):
+    finalMatrix = []
+    for row in matrix:
+        matrixElement = []
+        for element in row:
+            element *= multiplier
+            matrixElement.append(element)
+        finalMatrix.append(matrixElement)
+    print(finalMatrix)
+
+def main():
+    matrix = [[1,2,5], [12, 4, 5], [1, 4, 5]]
+    multiplier = 2
+
+    scalarMultiplication(matrix, multiplier)
+
+if __name__ == "__main__":
+    main()
