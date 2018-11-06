0fd2f089dd7bfd73f64437630922a78ef82ffc13
LuisTei17 <luisedteixeira@gmail.com>
2018-11-06 00:32:08 -0200

    Initialized with first algorith, matrices add

---
+++ matrices/addition.py
@@ -0,0 +1,39 @@
+def calculate(matrixOne, matrixTwo):
+    finalMatrix = []
+    indexRow = 0
+    indexElement = 0
+
+    for row in matrixTwo:
+        finalMatrixRow = []
+        indexElement = 0
+        for element in row:
+            sumElements = matrixOne[indexRow][indexElement] + matrixTwo[indexRow][indexElement]
+            finalMatrixRow.append(sumElements) 
+            indexElement += 1
+        finalMatrix.append(finalMatrixRow)
+        indexRow += 1
+
+    print finalMatrix
+
+
+def hasDifferentRowsNumber(matrixOne, matrixTwo):
+    for row in matrixOne:
+        for rowTwo in matrixTwo:
+            if (len(row) != len(rowTwo)):
+                return True
+    return False
+
+def main():
+    matrixOne = [[1, 2, 3],[4, 5, 10]]
+    matrixTwo = [[4, 12, 4],[9, 1, 2]]
+
+    hasDifferentColumnsNumber = len(matrixOne) != len(matrixTwo) 
+    if (hasDifferentColumnsNumber):
+        print('Error, matrices with different number of columns')
+    elif (hasDifferentRowsNumber(matrixOne, matrixTwo)):
+        print('Error, matrices with different number of rows')
+    else:
+        calculate(matrixOne, matrixTwo)
+
+if __name__ == "__main__":
+    main()
